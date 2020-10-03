3ef454fae8c5d973e157ea1a1b87ea9f5f11e3d6
TangXinGithub <48245380+TangXinGithub@users.noreply.github.com>
2020-10-03 12:43:52 +0800

    ui fram

--- src/pages/TSBMS/TSBMS.js
+++ src/pages/TSBMS/TSBMS.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import Meta from 'vue-meta'
 import TSBMS from './TSBMS.vue'
-
+import router from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.use(Meta)
 
 const app = new Vue({
-  ...TSBMS
+  ...TSBMS,
+  router
 })
 
 app.$mount('#app')
