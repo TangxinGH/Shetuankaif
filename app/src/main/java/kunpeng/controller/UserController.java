88cdefb11af6454f4f8e83822393bf1ae1f428cd
lbelieving <elfbelief@gmail.com>
2020-10-09 10:12:54 +0800

    实体类

---
+++ app/src/main/java/kunpeng/controller/UserController.java
@@ -0,0 +1,32 @@
+package kunpeng.controller;
+
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import service.UserService;
+
+public class UserController  {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/toIndex")
+    public String show() {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public String login(@RequestParam String username, @RequestParam String password) {
+        System.out.println("username:" + username + ", password:" + password);
+        User user = userService.login(username, password);
+        if (user != null) {
+
+            return "loginsucess";
+        } else {
+            return "loginError";
+        }
+    }
+}
