334dc353901886171299e5cb4a32b66955ea9d28
mai-tao <mrmaitao@163.com>
2020-10-12 19:11:52 +0800

    编写了CommentController，并测试通过，更新了结果码

--- app/src/main/java/kunpeng/controller/CommentController.java
+++ app/src/main/java/kunpeng/controller/CommentController.java
@@ -1,54 +1,110 @@
 package kunpeng.controller;
 
 import entity.User;
+import kunpeng.bean.CodeMsg;
+import kunpeng.bean.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 //import org.w3c.dom.Comment;
 import service.CommentService;
 import entity.Comment;
 
 import java.util.Date;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class CommentController {
 
     @Autowired
-    CommentService commentService;
+    private CommentService commentService;
+    private static final String IDRegex = "[1-9]\\d*";
 
-    @RequestMapping("/addComment")
+    @RequestMapping(value = "/addComment", method = RequestMethod.POST)
     @ResponseBody
-    public Integer addAComment(@RequestParam String sno,@RequestParam String sName,@RequestParam String commentContent,@RequestParam String actID){
-        //return "42";
-        Integer code = 0;
+    public Result<Boolean> addAComment(@RequestParam String sno, @RequestParam String sname, @RequestParam String commentContent, @RequestParam String actID){
+        if(!sno.matches(IDRegex)){
+            return Result.error(CodeMsg.HOME_WRONG_STUDENT_NUMBER_FORMAT);
+        }
+        else if (sname.isEmpty()){
+            return Result.error(CodeMsg.USERNAME_NO_EXIST);
+        }
+        else if (!actID.matches(IDRegex)){
+            return Result.error(CodeMsg.BAD_ACTIVITYID_FORMAT);
+        }
+        else if (commentContent.equals("") || commentContent.isEmpty()){
+            return Result.error(CodeMsg.HOME_STUDENT_COMMENT_CONTENT_EMPTY);
+        }
         System.out.println("Adding a comment");
         Comment comment = new Comment();
-        //comment.setCmt_Sname(user.getSname());
-        //comment.setSno(user.getSno());
-        comment.setCmt_Sname(sName);
+        comment.setCmt_Sname(sname);
         comment.setCmt_Date(new Date());
         comment.setCmt_Content(commentContent);
+        comment.setSno(Integer.valueOf(sno));
+        comment.setActID(Integer.valueOf(actID));
         try {
-            comment.setSno(Integer.parseInt(sno));
-            comment.setActID(Integer.parseInt(actID));
             commentService.addAComment(comment);
-            code = 40;
             System.out.println("插入成功！");
         }
         catch (Exception e){
-            if (e instanceof DataIntegrityViolationException || e instanceof ArithmeticException){
-                code = 41;
+            if (e instanceof DataIntegrityViolationException){//外键出现问题，证明用户/活动不在对应数据库
+                return Result.error(CodeMsg.DATA_ERROR);
             }
+
             else {
-                code = 42;
+                return Result.error(CodeMsg.HOME_STUDENT_COMMENT_ADD_ERROR);
             }
         }
-        return code; //不知道返回什么
+        return Result.success(CodeMsg.ADD_COMMENT_SUCCESSFULLY);
+    }
+
+    @RequestMapping(value = "/getComments",method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String,Object> getAllCommentsOfAnActivity(@RequestParam String actID){
+        Map<String,Object> resultMap = new HashMap<>();
+        if(!actID.matches(IDRegex)){
+            resultMap.put("resultCode",CodeMsg.BAD_ACTIVITYID_FORMAT);
+            resultMap.put("comments",null);
+            return resultMap;
+        }
+        try {
+            List<Comment> comments = commentService.getCommentByActivityID(Integer.valueOf(actID));
+            resultMap.put("resultCode",CodeMsg.GET_COMMENTS_SUCCESSFULLY);
+            resultMap.put("comments",comments);
+            return resultMap;
+        }
+        catch (Exception e){
+            resultMap.put("resultCode",CodeMsg.UNKNOWN_ERROR);
+            resultMap.put("comments",null);
+            return resultMap;
+        }
+    }
+
+
+    @RequestMapping(value = "/DelComment",method = RequestMethod.GET)
+    @ResponseBody
+    public Result<Boolean> DeleteAComment(String commentID){
+        if (!commentID.matches(IDRegex)){
+            return Result.error(CodeMsg.BAD_ACTIVITYID_FORMAT);
+        }
+        try {
+            int affected = commentService.deleteACommentByCommentID(Integer.parseInt(commentID));
+            if (affected >= 1){
+                return Result.success(CodeMsg.DELETE_COMMENT_SUCCESSFULLY);
+            }
+            else {
+                return Result.error(CodeMsg.DELETE_COMMENT_ERROR);
+            }
+        }
+        catch (Exception e){
+            return Result.error(CodeMsg.UNKNOWN_ERROR);
+        }
+
     }
 
 }
